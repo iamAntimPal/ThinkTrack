abd0f6a978987ace3007e13a6189013f24c42ce9
Antim Pal <134076504+iamAntimPal@users.noreply.github.com>
2025-04-07 22:26:54 +0530

    write that

--- budget/expenses/expence.py
+++ budget/expenses/expence.py
@@ -6,13 +6,25 @@ file_path = r'C:/Users/antim/OneDrive/Documents/GitHub/ThinkTrack/budget/expense
 # Data to add
 data = [
      ["Date", "Amount", "Category", "Account", "Note", "Description"],
-     ["2025-04-07", "50", "Groceries", "Credit Card", "Supermarket", "Weekly grocery shopping"],
-     ["2025-04-07", "20", "Transport", "Cash", "Bus fare", "Commute to work"]
+     ["2025-04-07", "60","Transport", "Cash", "Taxi", "Home to work"]
 ]
 
 # Write data to the CSV file
 with open(file_path, mode='w', newline='', encoding='utf-8') as file:
      writer = csv.writer(file)
      writer.writerows(data)
+
+print("Data added successfully!")
+
+# Data to add
+data1 = [
+     ["Date", "Amount", "Category", "Account", "Note", "Description"],
+     ["2025-04-07", "60","Transport", "Cash", "bus", "Home to work"]
+]
+
+# Write data to the CSV file
+with open(file_path, mode='w', newline='', encoding='utf-8') as file:
+     writer = csv.writer(file)
+     writer.writerows(data1)
 
 print("Data added successfully!")
