195df9f70509e9c971029db20dbbba8a81993ee3
Antim Pal <134076504+iamAntimPal@users.noreply.github.com>
2025-04-16 23:51:31 +0530

    ADDING

--- Daily Rutine/rutine.py
+++ Daily Rutine/rutine.py
@@ -1,8 +1,9 @@
 import csv
+import os
 from datetime import datetime
 
 def add_task_to_csv(date, time, task, category, notes):
-    file_path = 'rutine.csv'
+    file_path = os.path.join(os.path.dirname(__file__), 'rutine.csv')
     
     # Open the CSV file in append mode
     with open(file_path, mode='a', newline='') as file:
@@ -14,7 +15,7 @@ if __name__ == "__main__":
     date = input("Enter the date (YYYY-MM-DD): ") or datetime.now().strftime('%Y-%m-%d')
     time = input("Enter the time (e.g., 08:00 AM): ")
     task = input("Enter the task description: ")
-    category = input("Enter the category (e.g., Health, Work, Personal): ")
+    category = input("Enter the category (e.g., Health, Work, Personal, Aim, Study, Exercise): ")
     notes = input("Enter any additional notes: ")
 
     add_task_to_csv(date, time, task, category, notes)
